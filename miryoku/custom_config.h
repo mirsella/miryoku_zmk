ce017cc5aa234d5f1b2a5de0b77385ea0620cc33
mirsella <mirsella@protonmail.com>
2024-02-16 18:55:22 +0100

    initial commit for corne

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,10 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_KLUDGE_MOUSEKEYSPR
+
+/* #define MIRYOKU_ALTERNATIVES_TAP_COLEMAKDH \ */
+/* &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \ */
+/* &kp A,             &kp R,             &kp S,             &kp T,             &kp G,             &kp M,             &kp N,             &kp E,             &kp I,             &kp O,             \ */
+/* &kp Z,             &kp X,             &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         &kp DOT,           &kp SLASH,         \ */
+/* U_NP,              U_NP,              &kp ESC,           &kp SPACE,         &kp TAB,           &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP */
