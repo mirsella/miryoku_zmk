7e5c79609bdd8db94227884b8c771ae15a6b9c13
mirsella <mirsella@protonmail.com>
2024-02-20 23:50:36 +0100

    tweak mouse

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,2 +1,6 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
+
+#define MIRYOKU_KLUDGE_MOUSEKEYSPR
+#define ZMK_MOUSE_DEFAULT_MOVE_VAL 2000
+#define U_MOUSE_MOVE_TIME 1000
