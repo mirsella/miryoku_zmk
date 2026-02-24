91fdaf0910a3a8eaf1f10933abe9367615bb9c87
mirsella <mirsella@protonmail.com>
2026-02-24 15:51:41 +0100

    fix(mousekeys): restore custom pointer sensitivity
    Port custom mouse tuning from the removed miryoku_kludge_mousekeyspr.h path to miryoku_mousekeys.h used after CONFIG_ZMK_POINTING.
    
    Set move/scroll defaults to 4000/50 and reduce move time to 1000, then remove obsolete kludge overrides from custom_config.h.

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,8 +1,2 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
-
-#define MIRYOKU_KLUDGE_MOUSEKEYSPR
-// doesn't work here i dunno why, so i modified directly
-// miryoku_kludge_mousekeyspr.h
-/* #define ZMK_MOUSE_DEFAULT_MOVE_VAL 2000 */
-/* #define U_MOUSE_MOVE_TIME 1000 */
